9df82bcb9a958574129941747bb338f306568757
timpt0261 <rchavez0215@gmail.com>
2022-04-08 23:43:22 -0700

    Added on to main.js, Rocket.js, and Play.js

--- src/main.js
+++ src/main.js
@@ -10,3 +10,12 @@ let game = new Phaser.Game(config);
 // set UI sizes
 let borderUISize = game.config.height / 15;
 let borderPadding = borderUISize / 3;
+
+// reserve keyboard vars
+let keyF, keyR, keyLEFT, keyRIGHT;
+
+// define keys
+keyF = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F);
+keyR = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R);
+keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
+keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -11,6 +11,7 @@ class Play extends Phaser.Scene{
     }
 
     create(){
+        
         // place tile sprite
         this.starfield = this.add.tileSprite(0, 0, 640, 480, 'starfield').setOrigin(0, 0);
 
@@ -22,10 +23,14 @@ class Play extends Phaser.Scene{
         this.add.rectangle(0, game.config.height - borderUISize, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0, 0);
         this.add.rectangle(0, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);
         this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);
-                
+        
+        // add rocket(p1)
+        this.p1Rocket = new Rocket(this, game.config.width/2, game.config.height - borderUISize - borderPadding, 'rocket').setOrigin(0.5, 0);
+
     }
 
     update() {
         this.starfield.tilePositionX -= 4;
+        this.p1Rocket.update();
     }
 }
