e4b050e814d85b2688ad1d7a1fd3cb1264bd4f61
timpt0261 <rchavez0215@gmail.com>
2022-04-08 23:54:00 -0700

    Added spaceship and rocket

---
+++ src/prefabs/Spaceship.js
@@ -0,0 +1,19 @@
+class Rocket extends Phaser.GameObjects.Sprite{
+    constructor(scene, x, y, texture,frame){
+        super(scene,x,y,texture, frame);
+
+        // add object to existing scene
+        scene.add.existing(this);
+        this.points = pointValue;
+        this.moveSpeed = 3;
+    }
+
+    update(){
+        // left/right movement
+        this.x -= this.moveSpeed;
+        // wrap around from left edge to right edge
+        if(this.x <= 0 - this.width){
+            this.x = game.config.width;
+        }
+    }
+}
--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -30,7 +30,17 @@ class Play extends Phaser.Scene{
     }
 
     update() {
+        //add space background
         this.starfield.tilePositionX -= 4;
+
+        // add Rocket Ship
         this.p1Rocket.update();
+
+        // add spaceships (x3)
+        this.ship01 = new Spaceship(this, game.config.width + borderUISize*6, borderUISize*4, 'spaceship', 0, 30).setOrigin(0, 0);
+        this.ship02 = new Spaceship(this, game.config.width + borderUISize*3, borderUISize*5 + borderPadding*2, 'spaceship', 0, 20).setOrigin(0,0);
+        this.ship03 = new Spaceship(this, game.config.width, borderUISize*6 + borderPadding*4, 'spaceship', 0, 10).setOrigin(0,0);
+        
+        
     }
 }
