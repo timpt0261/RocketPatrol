7326fd0780d857dca26f90e9e76f8febadcdc1e9
timpt0261 <rchavez0215@gmail.com>
2022-04-08 23:00:39 -0700

    Added:   - Rocket.js   - Play.js code

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -11,7 +11,9 @@ class Play extends Phaser.Scene{
     }
 
     create(){
-        
+        // place tile sprite
+        this.starfield = this.add.tileSprite(0, 0, 640, 480, 'starfield').setOrigin(0, 0);
+
         // green UI background
         this.add.rectangle(0, borderUISize + borderPadding, game.config.width, borderUISize * 2, 0x00FF00).setOrigin(0, 0);
         
@@ -21,5 +23,9 @@ class Play extends Phaser.Scene{
         this.add.rectangle(0, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);
         this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);
                 
+    }
+
+    update() {
+        this.starfield.tilePositionX -= 4;
     }
 }
