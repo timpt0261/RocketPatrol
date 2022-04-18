952a5ae1c5474685d46a93613c202a2bc7f90b8c
timpt0261 <rchavez0215@gmail.com>
2022-04-18 00:14:21 -0700

    renamed photos

--- src/main.js
+++ src/main.js
@@ -3,12 +3,7 @@ let config = {
     width: 640,
     height: 480,
     scene: [Menu, Play, Modded_Play],
-    physics: {
-        default:'arcade',
-        arcade: { 
-            gravity: {y : 200},
-        }
-    }
+    
 }
 
 let game = new Phaser.Game(config);
